d5e833cff1adc64431a994ebf6ec8f90ba0669c6
Arthur Li <z22919456@gmail.com>
2022-05-25 16:36:51 +0800

    add autoload for all needed

--- lib/action_short_message.rb
+++ lib/action_short_message.rb
@@ -1,5 +1,19 @@
-require "action_short_message/engine"
+require 'action_short_message/engine'
+require 'action_short_message/delivery_methods'
+require 'action_short_message/log_subscriber'
+require 'action_short_message/message_delivery'
+require 'action_short_message/message'
+require 'action_short_message/rescuable'
+require 'action_short_message/short_message'
+require 'sms_provider/base'
 
 module ActionShortMessage
-  # Your code goes here...
+  extend ::ActiveSupport::Autoload
+
+  autoload :Base
+  autoload :DeliveryMethods
+  autoload :LogSubscriber
+  autoload :MessageDelivery
+  autoload :Recuable
+  autoload :ShortMessage
 end
