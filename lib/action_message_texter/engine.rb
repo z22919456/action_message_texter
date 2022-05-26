8b0de9d0cbf97cbb5d680293ed45139012e7e7df
Arthur Li <z22919456@gmail.com>
2022-05-26 09:24:32 +0800

    set config when active support on load

--- lib/action_message_texter/engine.rb
+++ lib/action_message_texter/engine.rb
@@ -8,5 +8,12 @@ module ActionMessageTexter
     initializer 'action_message_texter.logger' do
       ActiveSupport.on_load(:action_message_texter) { self.logger ||= Rails.logger }
     end
+
+    config.after_initialize do |app|
+      ActiveSupport.on_load(:action_message_txter) do
+        options = app.config.action_short_message
+        options.each { |k, v| send("#{k}=", v) }
+      end
+    end
   end
 end
