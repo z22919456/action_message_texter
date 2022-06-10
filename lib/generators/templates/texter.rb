b15e5bf46fd6be5ebab0769e9cb665b9e8487a37
Arthur Li <z22919456@gmail.com>
2022-06-10 10:08:41 +0800

    v0.0.10
    generator typo

--- lib/generators/templates/texter.rb
+++ lib/generators/templates/texter.rb
@@ -1,7 +1,7 @@
 class <%="#{class_name}"%>Texter < ActionMessageTexter::Base
   <%actions.each do |action| %>
     def <%=action%>
-      sms(to: "+886987654321")
+      text(to: "+886987654321")
     end
   <%end%>
 end
